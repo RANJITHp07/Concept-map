3668d2dcc6f1cbdbc99adb9d097e37cc2b71efbe
ranjithp07 <ranjith.p@neurosparkworks.com>
2025-01-06 17:51:06 +0530

    chore(ui): Organize image imports and constants for better management

--- packages/ui/src/components/LeftMenu.tsx
+++ packages/ui/src/components/LeftMenu.tsx
@@ -1,6 +1,7 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { images } from "../constant.js";
 
 interface MenuItem {
   logo: string;
@@ -8,36 +9,41 @@ interface MenuItem {
 }
 
 function LeftMenu() {
-  const [activeItem, setActiveItem] = useState<number | null>(null); 
+  const [activeItem, setActiveItem] = useState<number | null>(null);
   const Data: MenuItem[] = [
-    { logo: 'x', title: 'Dashboard' },
-    { logo: 'x', title: 'Reports' },
-    { logo: 'x', title: 'Profile' },
-    { logo: 'x', title: 'Contracts' },
-    { logo: 'x', title: 'Invoices' },
-    { logo: 'x', title: 'Projects' },
-    { logo: 'x', title: 'Tasks' },
-    { logo: 'x', title: 'Settings' },
+    { logo: "x", title: "Dashboard" },
+    { logo: "x", title: "Reports" },
+    { logo: "x", title: "Profile" },
+    { logo: "x", title: "Contracts" },
+    { logo: "x", title: "Invoices" },
+    { logo: "x", title: "Projects" },
+    { logo: "x", title: "Tasks" },
+    { logo: "x", title: "Settings" },
   ];
 
   const handleClick = (index: number) => {
-    setActiveItem(index); 
+    setActiveItem(index);
   };
 
   return (
     <section className="px-[20px] py-[20px]">
+      <img src={images.logo.src} alt="logo" />
       <div className="">
         {Data.map((item, index: number) => (
           <div
             key={index}
-            onClick={() => handleClick(index)} 
+            onClick={() => handleClick(index)}
             className={`flex items-center justify-start px-[20px] py-[10px] rounded-[12px] 
-              ${activeItem === index ? 'bg-[rgba(246,170,22,1)] text-white' : 'hover:bg-[rgba(246,170,22,1)]'}`}
+              ${activeItem === index ? "bg-[rgba(246,170,22,1)] text-white" : "hover:bg-[rgba(246,170,22,1)]"}`}
           >
-            <span className={`text-xl font-bold ${activeItem === index ? 'text-white' : 'text-gray-600'} mr-4`}>
+            <span
+              className={`text-xl font-bold ${activeItem === index ? "text-white" : "text-gray-600"} mr-4`}
+            >
               {item.logo}
             </span>
-            <span className={`text-[18px] leading-[21px] ${activeItem === index ? 'text-white' : 'text-gray-800'}`}>
+            <span
+              className={`text-[18px] leading-[21px] ${activeItem === index ? "text-white" : "text-gray-800"}`}
+            >
               {item.title}
             </span>
           </div>
