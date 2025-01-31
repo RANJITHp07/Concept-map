a70f6c32a4e01c77846e49e22a74b8fd729959e5
ranjithp07 <ranjith.p@neurosparkworks.com>
2025-01-31 15:30:12 +0530

    fixed the tailwind configuration

--- apps/creator/app/layout.tsx
+++ apps/creator/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import "./global.css";
+import "@repo/ui/globals.css";
 
 const roboto = Roboto({
   subsets: ["latin"],
